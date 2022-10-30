4ff2cdb008d32f85a560b97e332c2b08b744c4f9
Alain Herve <alain.1337dev@outlook.com>
2022-10-30 13:55:32 +0200

    Update Introduction.jsx

--- locomotive-scroll/src/components/intro/Introduction.jsx
+++ locomotive-scroll/src/components/intro/Introduction.jsx
@@ -1,26 +1,29 @@
-import image from '../../assets/profile.png';
-import './intro-style.css';
+import image from "../../assets/profile.png";
+import "./intro-style.css";
 
 const Introduction = () => {
-	return (
-		<section
-			className="intro-section"
-			data-scroll-section
-			data-scroll
-			data-scroll-speed="6"
-		>
-			<div className="intro-image">
-				<img src={image} height={700} alt="profile" />
-			</div>
-			<div className="intro-title">
-				<h1 data-scroll data-scroll-speed="9">
-					Isaac Junior
-					<br />A Frontend Engineer and Technical writer with a deep focus on
-					creating pixel perfect designs
-				</h1>
-			</div>
-		</section>
-	);
+  return (
+    <section
+      className="intro-section"
+      data-scroll-section
+      data-scroll
+      data-scroll-speed="6"
+    >
+      <div className="intro-image">
+        <img src={image} height={700} alt="profile" />
+      </div>
+      <div className="intro-title">
+        <h1 data-scroll data-scroll-speed="9">
+          I am Naruto Uzumaki
+          <br />
+          Don't underestimate me! I don't quit and I do't run! You can act tough
+          all you want! You're not gonna scare me off! No way! I don't care if I
+          do get stuck as a Genin for the rest of my life! I'll still be Hokage
+          someday.
+        </h1>
+      </div>
+    </section>
+  );
 };
 
 export default Introduction;
